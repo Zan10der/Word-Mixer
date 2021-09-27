42123df206f2efe47f7cb04d83117d7e942021e7
Zan10der <zvanheerden@live.co.za>
2021-09-28 00:11:06 +0200

    First React page render

--- alt_text/frontend/views.py
+++ alt_text/frontend/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-def main(request):
-    return response
+def index(request, *args, **kwargs):
+    return render(request, 'frontend/index.html')
