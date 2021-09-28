950a8a6a8f61c58999ad326f029d527465c564dd
Zan10der <zvanheerden@live.co.za>
2021-09-28 23:20:42 +0200

    Added Vue app and created Scrabble app part

--- alt_text/alt_text/settings.py
+++ alt_text/alt_text/settings.py
@@ -39,10 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'api.apps.ApiConfig',
+    'scrabble',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -53,6 +56,9 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'alt_text.urls'
+
+# This should be changed once this is in production
+CORS_ORIGIN_ALLOW_ALL = True
 
 TEMPLATES = [
     {
